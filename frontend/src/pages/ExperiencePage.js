4c568479e9f80512272a3ea0e036e489095a6d44
DanielArevalo059 <arevalodaniel91@gmail.com>
2024-08-16 21:01:21 -0700

    Added university logos and positioned on experience page.

--- frontend/src/pages/ExperiencePage.js
+++ frontend/src/pages/ExperiencePage.js
@@ -5,18 +5,19 @@ function ExperiencePage() {
   return (
     <>
       <article>
+        {/* Logo images */}
+        <p class="OSULogo">
+  <img src='../images/OSULogo.png' alt="OSU Logo" width="40%" height="40%"></img>
+</p>
+<p class="EBLogo">
+  <img src='../images/EastBayLogo.png' alt="East Bay Logo" width="45%" height="45%"></img>
+</p>
         <h2>Education</h2>
-
         <dl>
           <dt>
-            <strong>
-              <Link
-                to="https://developers.google.com/machine-learning/crash-course"
-                target="_blank"
-              >
-                Google Machine Learning Crash Course
-              </Link>
-            </strong>
+            <strong><u>
+          Google Machine Learning Crash Course
+            </u></strong>
           </dt>
           <dd>
             <li class="experience">In Progress</li>
@@ -40,10 +41,20 @@ function ExperiencePage() {
               </dd>
             </Link>
           </dt>
-
+          <p></p>
+          <dt>
+            <strong>
+              <u>Bachelor of Arts, Liberal Studies</u>
+            </strong>
+          </dt>
+          <dt>Fall 2015 - Winter 2017</dt>
+          <dt>Cal State East Bay, Hayward, CA</dt>
+          <dd>
+            <li class="experience">Relevant Coursework: Math Minor</li>
+          </dd>
           <table>
             <caption>
-              <strong>Technical Proficiencies Acquired</strong>
+              <strong>Technical Proficiencies</strong>
             </caption>
             <tr>
               <td>
@@ -90,17 +101,6 @@ function ExperiencePage() {
               </td>
             </tr>
           </table>
-          <p></p>
-          <dt>
-            <strong>
-              <u>Bachelor of Arts, Liberal Studies</u>
-            </strong>
-          </dt>
-          <dt>Fall 2015 - Winter 2017</dt>
-          <dt>Cal State East Bay, Hayward, CA</dt>
-          <dd>
-            <li class="experience">Relevant Coursework: Math Minor</li>
-          </dd>
           <p></p>
         </dl>
         <h2>Work Experience</h2>
