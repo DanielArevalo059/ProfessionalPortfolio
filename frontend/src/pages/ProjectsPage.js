ba9542e85c34234f4fe27272f849c13ba661f1b1
DanielArevalo059 <arevalodaniel91@gmail.com>
2024-08-20 22:15:35 -0700

    Projects page sidebar functional. Need to fill with project content from backend.

--- frontend/src/pages/ProjectsPage.js
+++ frontend/src/pages/ProjectsPage.js
@@ -1,18 +1,41 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import ProjectSidebar from "../components/ProjectSidebar";
 
 function ProjectsPage() {
   const [sidebarToggled, setSidebarToggled] = useState(false);
+  const sidebarRef = useRef(null);
 
+  useEffect(() => {
+    function handler(e) {
+      if (sidebarRef.current) {
+        if (
+          !e.target.classList.contains("sidebar") &&
+          !e.target.classList.contains("sidebar-toggle")
+        ) {
+          setSidebarToggled(false);
+        }
+      }
+    }
+
+    document.addEventListener("click", handler);
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  });
   return (
     <>
       <h2>Projects</h2>
-      <aside className={`${sidebarToggled ? "visible" : ""}`}>
+      <aside ref={sidebarRef} className={`${sidebarToggled ? "visible" : ""}`}>
         ProjectSidebar
       </aside>
       <article>
-        <button className="sidebar-toggle" onClick={() => setSidebarToggled(true)}>Project Description</button>
+        <button
+          className="sidebar-toggle"
+          onClick={() => setSidebarToggled(true)}
+        >
+          Project Description
+        </button>
       </article>
     </>
   );
