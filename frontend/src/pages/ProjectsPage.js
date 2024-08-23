0bb50a688b8e72350e31b040c540a1ea6558a1fc
DanielArevalo059 <114385405+DanielArevalo059@users.noreply.github.com>
2024-08-23 15:37:52 -0700

    Completed GalagaAR on project page and project aside with photos.

--- frontend/src/pages/ProjectsPage.js
+++ frontend/src/pages/ProjectsPage.js
@@ -41,55 +41,113 @@ function ProjectsPage() {
             <h3>Capstone Project: GalagaAR</h3>
           </dt>
           <em>Unreal Engine | Augmented Reality | Xcode</em>
-          <p>
-            <dd>
-              <strong>Collaborated</strong> with three classmates to develop a
-              robust AR version of the classic arcade game, Galaga.
-            </dd>
-          </p>
-          <p>
-            <dd>
-              <strong>Leveraged</strong> Unreal Engine's cutting-edge AR
-              libraries and blueprinting system to create a deployable iOS
-              application through Xcode.
-            </dd>
-          </p>
-          <p>
-            <dd>
-              <strong>Facilitated </strong>effective communication within the
-              team, ensuring alignment on project goals, timelines, and
-              technical challenges.
-            </dd>
-          </p>
-          <p>
-            <dd>
-              <strong>Contributed</strong> to all stages of the project
-              lifecycle, including design, development, testing, and deployment,
-              showcasing strong teamwork and problem-solving skills.
-            </dd>
-          </p>
-          <button
-            className="sidebar-toggle"
-            onClick={() => {
-              setProjectID(0);
-              setSidebarToggled(true);
-            }}
-          >
-            Show More
-          </button>
-          <p></p>
-          Project Poster:{" "}
-          <a href="../../images/CS367_Poster.jpg" target="_blank">
-            Available Here
-          </a>
-          <p></p>
-          GitHub Repository:{" "}
-          <a
-            href="https://github.com/psargeant/CS467_Capstone_Project"
-            target="_blank"
-          >
-            Available Here
-          </a>
+          <div className="experience">
+            <p>
+              <dd>
+                <strong>Collaborated</strong> with three classmates to develop a
+                robust AR version of the classic arcade game, Galaga.
+              </dd>
+            </p>
+            <p>
+              <dd>
+                <strong>Leveraged</strong> Unreal Engine's cutting-edge AR
+                libraries and blueprinting system to create a deployable iOS
+                application through Xcode.
+              </dd>
+            </p>
+            <p>
+              <dd>
+                <strong>Facilitated </strong>effective communication within the
+                team, ensuring alignment on project goals, timelines, and
+                technical challenges.
+              </dd>
+            </p>
+            <p>
+              <dd>
+                <strong>Contributed</strong> to all stages of the project
+                lifecycle, including design, development, testing, and
+                deployment, showcasing strong teamwork and problem-solving
+                skills.
+              </dd>
+            </p>
+          </div>
+          <table className="project-table">
+            <thead>
+              <tr>
+                <th style={{ width: "20%" }}></th>
+                <th style={{ width: "auto" }}></th>
+                <th style={{ width: "auto" }}></th>
+                <th style={{ width: "auto" }}></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <button
+                    className="sidebar-toggle"
+                    onClick={() => {
+                      setProjectID(0);
+                      setSidebarToggled(true);
+                    }}
+                  >
+                    View Full Desription
+                  </button>
+                </td>
+                <td
+                  rowSpan={"3"}
+                  style={{ textAlign: "center", verticalAlign: "middle" }}
+                >
+                  <img
+                    id="GalagaAR-Gameplay-1"
+                    src="../images/GalagaAR-Gameplay-1.png"
+                    alt="GalagaAR gameplay example"
+                    style={{ width: "60%", height: "auto" }}
+                  />
+                </td>
+                <td
+                  rowSpan={"3"}
+                  style={{ textAlign: "center", verticalAlign: "middle" }}
+                >
+                  <img
+                    id="GalagaAR-Gameplay-2"
+                    src="../images/GalagaAR-Gameplay-2.png"
+                    alt="GalagaAR gameplay example"
+                    style={{ width: "60%", height: "auto" }}
+                  />
+                </td>
+                <td
+                  rowSpan={"3"}
+                  style={{ textAlign: "center", verticalAlign: "middle" }}
+                >
+                  <img
+                    id="GalagaAR-Gameplay-3"
+                    src="../images/GalagaAR-Gameplay-3.png"
+                    alt="GalagaAR gameplay example"
+                    style={{ width: "60%", height: "auto" }}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Project Poster:{" "}
+                  <a href="../../images/CS367_Poster.jpg" target="_blank">
+                    Available Here
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  GitHub Repository:{" "}
+                  <a
+                    href="https://github.com/psargeant/CS467_Capstone_Project"
+                    target="_blank"
+                  >
+                    Available Here
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
           <p></p>
         </dl>
         <dl className="project-description">
